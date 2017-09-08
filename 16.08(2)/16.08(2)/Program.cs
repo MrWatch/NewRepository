b129076959413258c624e9e173c461b58a7e86b4
MrWatch <mrhuman00@gmail.com>
2017-09-08 21:15:23 +0600

    Add files via upload

---
+++ 16.08(2)/16.08(2)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._08_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальный индекс: ");
+            int fIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите конечный индекс: ");
+            int lIndex = Convert.ToInt32(Console.ReadLine());
+            if (lIndex < fIndex || lIndex == fIndex) { Environment.Exit(0); }
+            int Size = lIndex - fIndex + 1 ;
+            RangeOfArray array = new RangeOfArray(Size);
+            int num = 1337;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = num;
+                Console.Write("{0} - {1}\n",fIndex, array[i]);
+                fIndex++;
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 16.08(2)/16.08(2)/RangeOfArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _16._08_2_
+{
+    public class RangeOfArray
+    {
+        public int[] array;
+        public int Length;
+        public RangeOfArray(int Size)
+        {
+            array = new int[Size];
+            Length = Size;
+        }
+        public int this[int index]
+        {
+            set
+            {
+                index = value;
+            }
+            get
+            {
+                return array[index];
+            }
+        }
+    }
+}
