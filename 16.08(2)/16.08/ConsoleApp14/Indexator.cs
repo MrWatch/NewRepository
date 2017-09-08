ee636e0edd94a789fdeaf30a95627bdeea651d7b
MrWatch <mrhuman00@gmail.com>
2017-09-08 21:18:22 +0600

    Add files via upload

---
+++ 16.08(2)/16.08/ConsoleApp14/Indexator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Indexator
+    {
+        public int[] array;
+        public int Length;
+        public Indexator(int Size)
+        {
+            array = new int[Size];
+            Length = Size;
+        }
+
+        public int this[int index]
+        {
+            set
+            {
+                array[index] = value;
+            }
+
+            get
+            {
+                array[index] = array[index] * array[index];
+                return array[index];                
+            }
+        }
+    }
+}
---
+++ 16.08(2)/16.08/ConsoleApp14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indexator arr = new Indexator(5);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 2;
+                Console.Write("{0}\t", arr[i]);
+            }
+            Console.Read();
+        }
+    }
+}
