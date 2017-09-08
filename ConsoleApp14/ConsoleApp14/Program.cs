1d1829c8d2665b034cb4ffc16302f26ec978f927
MrWatch <mrhuman00@gmail.com>
2017-09-08 20:32:00 +0600

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://awdokanwdoawnodnawo.ru/");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                response.Close();
+            }catch { Console.WriteLine("Error 404"); }
+            
+
+            int[] array=new int[5];
+            try
+            {
+                for (int i = 0; i < 6; ++i)
+                {
+                    Console.Write(array[i]);
+                }
+            }
+            catch { Console.WriteLine("\nError 405"); }
+            Console.Read();
+        }
+    }
+}
