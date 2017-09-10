65f96645a59e33fc34790cc7b68e48dd58301eef
MrWatch <mrhuman00@gmail.com>
2017-09-10 16:50:58 +0600

    Add files via upload

---
+++ 23.08/23.08(2)/23.08(2)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._08_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>();
+            Random rand = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                int num = rand.Next(1, 100);
+                list.Add(num);
+            }
+            Console.WriteLine("*************");
+            foreach(int a in list)
+            {
+                Console.WriteLine(a);
+            }
+            
+            Console.WriteLine("Числa больше средне арифметического: ");
+            for (int i = 0; i < n; i++)
+            {
+                if (list[i] > list.Average()) { Console.WriteLine(list[i]); }
+            }
+            Console.WriteLine("*************");
+            for(int i = 0; i < n; ++i)
+            {
+                if (i % 2 != 0) { list.RemoveAt(i); }
+            }
+            foreach (int a in list)
+            {
+                Console.WriteLine(a);
+            }            
+            Console.Read();
+        }
+    }
+}
---
+++ 23.08/23.08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine("Введите количество чисел: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа: ");
+            for(int i = 0; i < n; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                list.Add(num);
+            }
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0) { sum += list[i]; }
+            }
+            list.Remove(list.Max());
+            Console.WriteLine("Второе максимальное число: {0} - {1}" ,list.IndexOf(list.Max()), list.Max());
+            Console.WriteLine("Сумма четных элементов массива: " + sum);
+            Console.Read();
+        }
+    }
+}
