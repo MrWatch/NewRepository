0d112fd2e3cb0b071088d13b6f708e9d8451604a
MrWatch <mrhuman00@gmail.com>
2017-09-11 19:44:40 +0600

    Add files via upload

---
+++ homework/homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\n-\t\t-\t\t-| HOMEWORK DONE |-\t\t-\t\t-");
+            Console.Read();
+        }
+    }
+}
