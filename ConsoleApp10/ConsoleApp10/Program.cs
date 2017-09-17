5cc8560d00b617e3a72a73d8b32b888cecf21daa
MrWatch <mrhuman00@gmail.com>
2017-09-17 12:58:24 +0600

    07.09

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> list = new List<Item>();
+
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load("https://habrahabr.ru/rss/interesting/");
+
+            XmlElement rights = xdoc.DocumentElement;
+
+            foreach (XmlElement xmlDoc in rights)
+            {
+                foreach (XmlElement elem in xmlDoc)
+                {
+                    if (elem.Name.Equals("Item"))
+                    {
+                        Item item = new Item();
+                        item.Title = elem["title"].InnerText;
+                        item.Link = elem["link"].InnerText;
+                        item.Description = elem["description"].InnerText;
+                        item.PubDate = DateTime.Parse(elem["pubDate"].InnerText);
+                        list.Add(item);
+                    }
+                }
+            }
+
+            foreach (Item item in list)
+            {
+                Console.WriteLine("\n\n\t\t\t\n\n");
+                Console.WriteLine(item.Title + "\n" + item.Description + " \n" + item.Link + "\n" + item.PubDate);
+            }
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Student.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "grade")
+                    {
+                        Console.WriteLine("Класс: {0}", childnode.InnerText);
+                    }
+                    if (childnode.Name == "letter")
+                    {
+                        Console.WriteLine("Буква: {0}", childnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            XmlDocument document = new XmlDocument();
+            document.Load("Request.xml");
+
+            XmlNode root = document.DocumentElement;
+
+                           
+            Requests request = new Requests();
+            request.ForwardUrl = root["forwardUrl"].InnerText;
+            request.Language = root["language"].InnerText;
+                   
+            request.UserId = long.Parse(root["userId"].InnerText);
+
+            Console.WriteLine(root.Name);
+            foreach (XmlNode child in root.ChildNodes)
+                {              
+                   Console.WriteLine("  " + child.Name);
+
+                if (child.HasChildNodes)
+                {
+                    foreach (XmlNode child2 in child.ChildNodes)
+                    {
+                        if (child2.HasChildNodes)
+                        {
+                            Console.WriteLine("    " + child2.Name);  
+                            foreach (XmlNode child3 in child2.ChildNodes)
+                            {                            
+                                 Console.WriteLine("      " + child3.Name);
+                            }                                              
+                        }                        
+                    }                    
+                }                
+            }                                     
+            Console.ReadLine();          
+        }
+    }
+}
